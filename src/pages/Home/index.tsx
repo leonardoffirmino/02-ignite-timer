51f39f71e316418a24b9b829294f0ab0891c8d35
leonardoffirmino <firmino750@gmail.com>
2024-01-17 23:02:55 -0300

    Finished home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <FormsContainer>
           <label htmlFor="taks">Vou trabalhar em </label>
-          <TaksInput id="taks" placeholder="Dê um nome para o seu projeto" />
+          <TaksInput
+            id="taks"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAMount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAMount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
